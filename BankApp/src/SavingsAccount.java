94880f9d6506702a48986978b7903530986b70a5
Siboniso Mabuza <sibonisot@yahoo.com>
2023-01-05 10:42:25 +0200

    Delete BankApp directory

--- BankApp/src/SavingsAccount.java
+++
@@ -1,5 +0,0 @@
-public class SavingsAccount extends Account{
-    public SavingsAccount(String accountNum, String accountType, double balance) {
-        super(accountNum, accountType, balance);
-    }
-}
