be3082647e65466f8d8cb61b7773933a771bc1aa
Siboniso Mabuza <sibonisot@yahoo.com>
2023-01-06 08:28:24 +0200

    Delete BankApp directory

--- BankApp/src/IUSSD.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public interface IUSSD {
-    public Account userAuth() throws IOException;
-    public void startApplication() throws IOException;
-    public void exit();
-    public int appMenu();
-    public int authMenu() throws IOException;
-}
